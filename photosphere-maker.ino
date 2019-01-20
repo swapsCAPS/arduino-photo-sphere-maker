b0949c88c588f8167a977bc79554a30540db4b26
swapsCAPS <verstegen.daan@gmail.com>
2019-01-20 18:49:49 +0100

    accelstepper lib much better : )

--- photosphere-maker.ino
+++ photosphere-maker.ino
@@ -1,25 +1,49 @@
 #include <AccelStepper.h>
 #include <Servo.h>
 
-AccelStepper pan(AccelStepper::FULL4WIRE, 8, 10, 9, 11);
+AccelStepper pan(AccelStepper::HALF4WIRE, 8, 10, 9, 11);
+
+#define stepsPerRev   4096
+#define outerTeeth    107
+#define innerTeeth    15
+
+float ratio;
+float stepsPerTTRev;
 
 void setup() {
   Serial.begin(9600);
-  pan.setMaxSpeed(17.0);
-  pan.setAcceleration(100.0);
+  ratio         = outerTeeth / innerTeeth;
+  stepsPerTTRev = stepsPerRev * ratio;
   pan.setCurrentPosition(0);
+  pan.setMaxSpeed(1400.0);
+  pan.setAcceleration(1000.0);
+  Serial.println("enter degrees °");
 }
 
-int panPos;
+int currentPos = 0;
+int val;
+int req;
+
 void loop() {
   while (Serial.available() > 0) { // Something is in the serial buffer
-    panPos = Serial.parseInt();
-    String toPrint = "received ";
-    toPrint += panPos;
+    val = Serial.parseInt();
+    currentPos = val;
+
+    val *= -1;
+
+    req = map(val, 0, 359, 0, stepsPerTTRev);
+
+    String toPrint = "Moving ";
+    toPrint += val;
+    toPrint += "°";
+    if (val > 0) {
+      toPrint += " ccw";
+    } else {
+      toPrint += " cw";
+    }
+
     Serial.println(toPrint);
-    delay(1000);
-    Serial.println("moving");
-    pan.moveTo(panPos);
-    Serial.println("done");
+    pan.move(req);
   }
+  pan.run();
 }
