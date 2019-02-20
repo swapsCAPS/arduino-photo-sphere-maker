99f9f5759eaa17ce72a7af38541266e32de9d311
swapsCAPS <verstegen.daan@gmail.com>
2019-02-20 22:38:05 +0100

    support offset moves for better photo alignment

--- photosphere-maker.ino
+++ photosphere-maker.ino
@@ -21,30 +21,45 @@ void setup() {
 
   pan.setCurrentPosition(0);
   pan.setMaxSpeed(250.0);
-  pan.setAcceleration(250.0);
+  pan.setAcceleration(100.0);
+
   servoL.attach(6);
   servoR.attach(7);
-  servoL.write(0);
-  servoR.write(0);
+  servoL.write(92);
+  servoR.write(92);
+  delay(2000);
   servoL.detach();
   servoR.detach();
-
 }
 
 bool started = false;
 
-float degrees[] = { 30.0, 40.0, 120.0 };
-/* int amounts[]   = { 12, 9, 3 }; */
-int amounts[]   = { 1, 1, 1 };
-float tiltAngles[] = { 90, 55, 25 };
+  // Degrees:
+  // 120 *  3 @ 25
+  // 40  *  9 @ 55
+  // 30  * 12 @ 90
+  // 40  *  9 @ 125 ?
+  // 120 *  3 @ 155 ?
+float degrees[]    = { 30.0, 40.0, 120.0,  40.0, 120.0 };
+float offsets[]    = {  0,  -15.0,   0,     0,     0   };
+int amounts[]      = { 1,    1,     1,     1,     1   };
+/* int amounts[]      = { 11,    9,     3,     9,     3   }; */
+float tiltAngles[] = { 92,   57,    23,   130,   155   };
 
 int step   = 0;
 int amount = 0;
+
+bool offsetting = false;
 
 void loop() {
   pan.run();
   while (Serial.available() > 0) { // Something is in the serial buffer
     String str  = Serial.readString();
+
+    if (str == "stop") {
+      started = false;
+      pan.stop();
+    }
 
     if (str == "start") {
       started = true;
@@ -53,8 +68,6 @@ void loop() {
       servoR.attach(7);
       servoL.write(abs(tiltAngles[step] - 180));
       servoR.write(tiltAngles[step]);
-      servoL.detach();
-      servoR.detach();
 
       float req = (stepsPerTTRev / 360.0) * degrees[amount];
       Serial.println(amount);
@@ -63,32 +76,48 @@ void loop() {
   }
 
   if (started == true) {
+
+    if (offsetting == true) {
+      if (abs(pan.distanceToGo()) > 0) { return; } // Still need to move
+      offsetting = false;                          // No more movement, stop offsetting
+      return;
+    }
+
     if (amount < amounts[step]) {
-      if (pan.distanceToGo() <= 0) {
-        amount++;
-        Serial.println("status");
-        Serial.println(step);
-        Serial.println(amount);
-        Serial.println("");
-        float req = (stepsPerTTRev / 360.0) * degrees[step];
-        delay(500);
-        pan.move(req);
-      }
+      if (abs(pan.distanceToGo()) > 0) { return; } // Still need to move
+
+      // Movement done do next move
+      amount++;
+      float req = (stepsPerTTRev / 360.0) * degrees[step];
+      delay(500);
+      pan.move(req);
+
     } else {
-      step++;
+      step++; // Reached end of the steps on this level go to next
+
+      if (step > 4) { // Reached the end
+        started = false;
+        pan.stop();
+        servoL.attach(6);
+        servoR.attach(7);
+        servoL.write(abs(92 - 180));
+        servoR.write(92);
+
+        return Serial.println("Done");
+      }
+
       amount = 0;
       servoL.attach(6);
       servoR.attach(7);
       servoL.write(abs(tiltAngles[step] - 180));
       servoR.write(tiltAngles[step]);
-      servoL.detach();
-      servoR.detach();
+
+      delay(1000);
+
+      // Do offset move if we need it
+      offsetting = true;
+      float req = (stepsPerTTRev / 360.0) * offsets[step];
+      pan.move(req);
     }
   }
-  // Degrees:
-  // 120 *  3 @ 25
-  // 40  *  9 @ 55
-  // 30  * 12 @ 90
-  // 40  *  9 @ 125 ?
-  // 120 *  3 @ 155 ?
 }
